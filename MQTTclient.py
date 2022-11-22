182ca71d7a22c0db6b7028e03aa711c1e0db37f5
Huy Luu <huyluu1820@gmail.com>
2022-11-22 21:15:29 +0700

    Added two more options which are: resume and pause to test

--- MQTTclient.py
+++ MQTTclient.py
@@ -8,8 +8,10 @@ class MQTTclient:
         self.broker = broker
         self.port = port
         self.client_id = client_id
-        self.pid_to_kill = 0
+        self.pid_to_perform_on = 0
         self.kill_service = False
+        self.pause_service = False
+        self.resume_service = False
 
     def connect(self):
         self.client = mqttclient.Client(self.client_id)
@@ -24,23 +26,34 @@ class MQTTclient:
     def onMessage(self, client, userdata, message):
         global info
         info = str(message.payload.decode("utf-8"))
+        p = subprocess.Popen(['ps', '-To', 'pid:1,cmd:1'], stdout=subprocess.PIPE)
+
+        # Getting PID
+        out, err = p.communicate()
+        for line in out.splitlines():
+            if b'MotionPlanning.py' in line:
+                #print("Found service")
+                line = line.decode("utf-8")
+                print(line)
+                #print(line.split(None, 1)[0])
+                print(len(line.split(None, 1)[1]))
+                if(len(line.split(None, 1)[1]) > 60):
+                    pid = int(line.split(None, 1)[0])
+                    self.pid_to_perform_on = pid
+
+        # Process message
         if (message.topic == "control/service"):
             if(info == "k-service"):
                 print("Got the message to kill service")
-                p = subprocess.Popen(['ps', '-To', 'pid:1,cmd:1'], stdout=subprocess.PIPE)
-                out, err = p.communicate()
-                for line in out.splitlines():
-                    if b'MotionPlanning.py' in line:
-                        #print("Found service")
-                        line = line.decode("utf-8")
-                        print(line)
-                        #print(line.split(None, 1)[0])
-                        print(len(line.split(None, 1)[1]))
-                        if(len(line.split(None, 1)[1]) > 60):
-                            pid = int(line.split(None, 1)[0])
-                            self.pid_to_kill = pid
-                            #print(pid)
-                            self.kill_service = True
+                self.kill_service = True
+            
+            elif(info == "p-service"):
+                print("Got the message to pause the service")
+                self.pause_service = True
+
+            elif(info == "r-service"):
+                print("Got the message to resume the service")
+                self.resume_service = True
                 
 
     def loop_start(self):
